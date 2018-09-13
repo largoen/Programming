614bb0aa23b6769fedee015135dd2813ba099fdc
Largoen <largoen.tamana@student.hu.nl>
2018-09-13 15:37:56 +0200

    Test

---
+++ Programming les 2.py
@@ -0,0 +1,24 @@
+s1='-'
+s2='+'
+print(s1+s2)
+print(s1+s2+s1)
+print(s2+s1+s1)
+print(2*(s2+s1+s1))
+print(10*(s2+s1+s1))
+print(5*(s2+s1+s2+s2+s2+s1+s1+s2+s1))
+
+s='abcdefghijklmnopqrstuvwxyz'
+print(s[0])
+print(s[2])
+print(s[25])
+print(s[24])
+print(s[16])
+
+s='goodbye'
+print(s[0]=='g')
+print(s[6]=='g')
+print(s[0 and 1]=='g and a')
+print(s[5]=='x')
+print(s[3]=='d')
+print(s[0 and 6]== 'g and g')
+print([3 and 4 and 5 and 6]== 'tion')
